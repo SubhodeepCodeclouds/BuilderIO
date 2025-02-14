55e2c6fb817aa81ec57d9824865b0174bd05f463
Subhodeep Bhattacharjee <subhodeep.bhattacharjee@codeclouds.com>
2025-02-14 16:10:24 +0530

    remove link

--- src/builder-registry.ts
+++ src/builder-registry.ts
@@ -1,7 +1,6 @@
 "use client";
 import { builder, Builder } from "@builder.io/react";
 import Counter from "./components/Counter/Counter";
-// import Page from "./app/_old_pages_1/__[...page]";
 import Page from "./app/[...page]/page";
 
 builder.init(process.env.NEXT_PUBLIC_BUILDER_API_KEY!);
